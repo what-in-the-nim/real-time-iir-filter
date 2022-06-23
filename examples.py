02854d2734fdf967168549ac129e1d3efeaa6aa8
Win <chatcharinsang@gmail.com>
2022-06-23 21:14:39 +0700

    Move code under __name__ == '__main__'

--- examples.py
+++ examples.py
@@ -1,9 +1,5 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy.signal import welch
 from typing import Sequence
-
-from iir import IIR
 
 
 def signal_generator(signal_band: Sequence[tuple], shape: tuple, fs: int) -> tuple[np.ndarray]:
@@ -20,64 +16,69 @@ def signal_generator(signal_band: Sequence[tuple], shape: tuple, fs: int) -> tup
     return times, signals
 
 
-# Constant
-fs = 250
-length = 500
-num_channel = 2
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    from scipy.signal import welch
+    from iir import IIR
 
-# Creating IIR instance
-iir_filter = IIR(
-    num_channel=num_channel,
-    sampling_frequency=fs
-)
-iir_filter.add_filter(order=6, cutoff=(45, 55), filter_type='bandstop')
-iir_filter.add_filter(order=2, cutoff=30, filter_type='lowpass')
-iir_filter.add_filter(order=4, cutoff=3, filter_type='highpass')
+    # Constant
+    fs = 250
+    length = 500
+    num_channel = 2
 
-# Signal generation
-signal_band = [
-    (0.01, 200),
-    (8, 5),
-    (50, 20),
-    (80, 5)
-]
+    # Creating IIR instance
+    iir_filter = IIR(
+        num_channel=num_channel,
+        sampling_frequency=fs
+    )
+    iir_filter.add_filter(order=6, cutoff=(45, 55), filter_type='bandstop')
+    iir_filter.add_filter(order=4, cutoff=40, filter_type='lowpass')
+    iir_filter.add_filter(order=4, cutoff=3, filter_type='highpass')
 
-times, signals = signal_generator(
-    signal_band, shape=(length, num_channel), fs=fs)
+    # Signal generation
+    signal_band = [
+        (0.01, 200),
+        (8, 5),
+        (50, 20),
+        (80, 5)
+    ]
 
-# Input as numpy array
-filtered = iir_filter.filter(signals)
-# Input as list
-filtered = iir_filter.filter(signals.tolist())
+    times, signals = signal_generator(
+        signal_band, shape=(length, num_channel), fs=fs)
 
-# Plotting
-labels = [f'CH{num+1}' for num in range(num_channel)]
+    # Input as numpy array
+    filtered = iir_filter.filter(signals)
+    # Input as list
+    filtered = iir_filter.filter(signals.tolist())
 
-plt.subplot(2, 2, 1)
-plt.xlabel('Times (s)')
-plt.ylabel('Amplitude')
-plt.plot(times, signals)
-plt.legend(labels, loc=1)
+    # Plotting
+    labels = [f'CH{num+1}' for num in range(num_channel)]
 
-plt.subplot(2, 2, 2)
-plt.xlabel('Times (s)')
-plt.ylabel('Amplitude')
-plt.plot(times, filtered)
-plt.legend(labels, loc=1)
+    plt.subplot(2, 2, 1)
+    plt.xlabel('Times (s)')
+    plt.ylabel('Amplitude')
+    plt.plot(times, signals)
+    plt.legend(labels, loc=1)
 
-plt.subplot(2, 2, 3)
-bins, power = welch(signals.T, fs=fs, nperseg=240, noverlap=200, nfft=500)
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Magnitude (dB)')
-plt.plot(bins, 20*np.log10(power.T))
-plt.legend(labels, loc=1)
+    plt.subplot(2, 2, 2)
+    plt.xlabel('Times (s)')
+    plt.ylabel('Amplitude')
+    plt.plot(times, filtered)
+    plt.legend(labels, loc=1)
 
-plt.subplot(2, 2, 4)
-bins, power = welch(filtered.T, fs=fs, nperseg=240, noverlap=200, nfft=500)
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Magnitude (dB)')
-plt.plot(bins, 20*np.log10(power.T))
-plt.legend(labels, loc=1)
+    plt.subplot(2, 2, 3)
+    bins, power = welch(signals.T, fs=fs, nperseg=240, noverlap=200, nfft=500)
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Magnitude (dB)')
+    plt.plot(bins, 20*np.log10(power.T))
+    plt.legend(labels, loc=1)
 
-plt.tight_layout()
-plt.show()
+    plt.subplot(2, 2, 4)
+    bins, power = welch(filtered.T, fs=fs, nperseg=240, noverlap=200, nfft=500)
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Magnitude (dB)')
+    plt.plot(bins, 20*np.log10(power.T))
+    plt.legend(labels, loc=1)
+
+    plt.tight_layout()
+    plt.show()
